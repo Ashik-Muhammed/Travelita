7a536d7f970143e8dff5d3a3176e9b79facb44b4
Ashik Muhammed <ashikmuhammed3182@gmail.com>
2025-05-23 01:01:37 +0530

    Updates

---
+++ assets/Bookings-Cbq9Havj.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-jW0ZXYkQ.js";import"./router-CE_eIJm-.js";import"./react-CMmhtoO5.js";const r=()=>o.jsx("h1",{children:"Your Bookings"});export{r as default};
--- assets/Bookings-CsAfygH0.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Byul4zVF.js";import"./router-CE_eIJm-.js";import"./react-CMmhtoO5.js";const r=()=>o.jsx("h1",{children:"Your Bookings"});export{r as default};
